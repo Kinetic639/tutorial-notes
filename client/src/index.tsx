e53315d6a07ddb00e5eb0b14ed65bcf8a7478c92
Kinetic639 <michal.stepien36@gmail.com>
2022-07-07 11:53:36 +0200

    added basic dashboard layout and router to it

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {store} from './redux/app/store';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import {CssBaseline} from "@mui/material";
+import {BrowserRouter as Router} from 'react-router-dom'
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <CssBaseline/>
+                <App/>
+            </Router>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
