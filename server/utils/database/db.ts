63a2f0e5edd12c629a87bf898bb7560e4c557f54
Kinetic639 <michal.stepien36@gmail.com>
2022-07-15 22:19:01 +0200

    added password field to .env

--- server/utils/database/db.ts
+++ server/utils/database/db.ts
@@ -7,6 +7,7 @@ dotenv.config()
 export const pool = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     namedPlaceholders: true,
     decimalNumbers: true
